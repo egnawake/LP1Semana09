5b11fc465ac6fa92c626b4cc81f02681fd6b9bb6
João Inácio <egnawake@gmail.com>
2022-05-10 20:34:57 +0100

    Test Animal classes

--- AnimInterf/Program.cs
+++ AnimInterf/Program.cs
@@ -2,11 +2,49 @@
 
 namespace AnimInterf
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Animal[] animals = new Animal[10];
+
+            // Generate random animals
+            Random rnd = new Random();
+            for (int i = 0; i < animals.Length; i++)
+            {
+                int choice = rnd.Next(4);
+                animals[i] = choice switch
+                {
+                    0 => new Dog(),
+                    1 => new Cat(),
+                    2 => new Bat(),
+                    3 => new Bee(),
+                    _ => null
+                };
+            }
+
+            // Print animal details
+            foreach (Animal a in animals)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(a.Sound());
+                
+                if (a is IMammal)
+                {
+                    IMammal mammal = (IMammal)a;
+                    Console.WriteLine("Number of nipples: {0}",
+                        mammal.NumberOfNipples);
+                }
+
+                if (a is ICanFly)
+                {
+                    ICanFly f = (ICanFly)a;
+                    Console.WriteLine("Number of wings: {0}",
+                        f.NumberOfWings);
+                }
+
+                Console.WriteLine("");
+            }
         }
     }
 }
