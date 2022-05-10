49fcd0a108d1fa152ef18ef2b993a8f3157ab715
João Inácio <egnawake@gmail.com>
2022-05-11 00:58:44 +0100

    Implement adding new players

--- PlayerManager1/Program.cs
+++ PlayerManager1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PlayerManager1
 {
@@ -6,7 +7,39 @@ namespace PlayerManager1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Player> players = new List<Player>
+            {
+                new Player("Joe", 4),
+                new Player("Steve", 10)
+            };
+
+            string op = "";
+            while (op != "quit")
+            {
+                Console.Write("> ");
+                op = Console.ReadLine();
+
+                if (op == "add")
+                {
+                    Console.Write("Enter name: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Enter score: ");
+                    int score = int.Parse(Console.ReadLine());
+
+                    players.Add(new Player(name, score));
+
+                    Console.WriteLine($"Added {name} with {score} points");
+                }
+                else if (op == "quit")
+                {
+                    Console.WriteLine("Bye!");
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized command");
+                }
+            }
         }
     }
 }
