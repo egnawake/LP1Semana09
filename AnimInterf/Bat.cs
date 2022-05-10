332d393071d470fb39b9170ceb04e53280ef3cb4
João Inácio <egnawake@gmail.com>
2022-05-10 19:25:42 +0100

    Implement ICanFly in Bat class

--- AnimInterf/Bat.cs
+++ AnimInterf/Bat.cs
@@ -1,8 +1,10 @@
 namespace AnimInterf
 {
-    public class Bat : Animal, IMammal
+    public class Bat : Animal, IMammal, ICanFly
     {
         public int NumberOfNipples { get; } = 2;
+        public int NumberOfWings { get; } = 2;
+        
         public override string Sound()
         {
             return base.Sound() + "Cheep cheep";
